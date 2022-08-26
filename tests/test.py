21a0abe05da9bda833d5a8d672a544252303d150
ShreyaMaheshwari <shreya.2506@gmail.com>
2022-08-26 23:26:06 +0530

    Demonstrating __init__.py functionality

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,5 @@
 import pytest
+import src
 
 def inc(x):
     return x + 1
@@ -6,3 +7,6 @@ def inc(x):
 
 def test_answer():
     assert inc(3) == 4
+
+def test_answer_subtraction():
+    assert src.subtraction(5, 3) == 2
